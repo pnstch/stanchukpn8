dde754294497af1f4994a47fe7689138f0900324
Полина Николаевна <85806007+pnstch@users.noreply.github.com>
2022-12-09 01:41:46 +0300

    Контрольная 8

---
+++ src/Answer.java
@@ -0,0 +1,28 @@
+public class Answer {
+
+    public Answer() {
+    }
+
+    public Answer(Question question, int yourAnswer) {
+        this.yourAnswer = yourAnswer;
+        this.question = question;
+    }
+
+    public boolean checkCorrect() {
+        if (question.getRightAnswer() == yourAnswer)
+            return true;
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Answer{" +
+                "yourAnswer=" + yourAnswer +
+                ", question=" + question +
+                '}';
+    }
+
+    private int yourAnswer;
+    private Question question;
+
+}
